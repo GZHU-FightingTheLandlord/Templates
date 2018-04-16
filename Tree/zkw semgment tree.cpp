2487ce16a3ace522eccd12b5fa7f3f4252d97c46
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-04-16 23:32:35 +0800

    Update zkw semgment tree.cpp

--- Tree/zkw semgment tree.cpp
+++ Tree/zkw semgment tree.cpp
@@ -15,14 +15,14 @@ public:
 		while(_len>=t)t*=2;
 		len=t;
 		data=new int64 [len*2+5];
-		//if(data==NULL)throw runtime_error("Memory Limit Exceeded or some other error.");
+		//if(data==NULL)throw std::runtime_error("Memory Limit Exceeded or some other error.");
 	}
 	void update(int64 i,int64 x)
 	{
 		data[i+=len]+=x;
 		for(i>>=1;i;i>>=1)data[i]=how_to_update(data[i<<1],data[i<<1|1]);
 	}
-	int64 query(int s,int t)
+	int64 query(int s,int t)/// must make sure that s<t
 	{
 		int64 ans=0; /** there is a point to recompose while you are not update a summation. **/
 		for(s+=len-1,t+=len+1;s^t^1;s>>=1,t>>=1)
@@ -30,6 +30,7 @@ public:
 			if(~s&1)ans=how_to_update(ans,data[s^1]);
 			if(t&1)ans=how_to_update(ans,data[t^1]);
 		}
+		return ans;
 	}
 private:
 	int len;
@@ -39,4 +40,8 @@ private:
 /** This code is not to be used yet.
 	Welcome to hack it. **/
 
-
+/** create a zkw_tree: zkw_tree treea(1000);
+	下标从1开始。the subscript is from 1.
+	更新树的下标为location为value: object.zkw_tree::update(location,value);
+	查询区间[from,to]: object.zkw_tree::query(from,to)
+**/
