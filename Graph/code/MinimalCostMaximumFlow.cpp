1585391dc4eaceed78e4a07ee7b426771b817766
Dumpling_s <35555233+semon0x29a@users.noreply.github.com>
2018-04-22 20:25:00 +0800

    给code添加注释

--- Graph/code/MinimalCostMaximumFlow.cpp
+++ Graph/code/MinimalCostMaximumFlow.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const int MAX = 1000;
 const int infi = (1 << 31) - 1;
 
-int n, m, cnt;
+int cnt;
 
 struct edge{
     int v, w, c, next;
@@ -14,28 +14,31 @@ struct edge{
     edge(int vv, int ww, int cc, int nn):v(vv), w(ww), c(cc), next(nn){}
 };
 
-edge e[MAX * MAX];
-int head[MAX];
-int pre[MAX];
-int cost[MAX];
+edge e[MAX * MAX]; // 边集
+int head[MAX];     // u为起点的第一条边在边集中的位置
+int pre[MAX];      // 父边
+int cost[MAX];     // 费用
 int inq[MAX];
 
-void init()
+void init() // 初始化
 {
     cnt = 0;
     memset(head, -1, sizeof head);
 }
 
-void addedge(int u, int v, int w, int c)
+void addedge(int u, int v, int w, int c) //加边
 {
+    // 正向边
     e[cnt] = edge(v, w, c, head[u]); head[u] = cnt++;
+    // 反向边
     e[cnt] = edge(u, 0, -c, head[v]); head[v] = cnt++;
+    // 对任意边i，i^1即为其逆向边
 }
 
-bool spfa(int from, int to)
+bool spfa(int from, int to)// spfa求最小费用增广路
 {
     queue<int> q;
-    for (int i = 0; i < n + 5; i++)
+    for (int i = 0; i < MAX; i++)
         cost[i] = infi, pre[i] = -1, inq[i] = 0;
     q.push(from);
     inq[from] = 1; cost[from] = 0;
@@ -59,7 +62,7 @@ bool spfa(int from, int to)
     return cost[to] != infi;
 }
 
-int augment(int from, int to)
+int augment(int from, int to) // 增广
 {
     int now = pre[to], flow = infi;
     while (now != -1)
@@ -80,7 +83,7 @@ int augment(int from, int to)
 int MinCostFlow(int from, int to)
 {
     int ans = 0;
-    while (spfa(from, to))
+    while (spfa(from, to)) // 持续增广直至无法找到增广路
         ans += augment(from, to);
     return ans;
 }
