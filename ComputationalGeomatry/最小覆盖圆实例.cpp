bc748f89256e01975aa1b9caa7df08b20feb5e39
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-05-02 14:27:12 +0800

    Update and rename B.cpp to 最小覆盖圆实例.cpp

--- ComputationalGeomatry/最小覆盖圆实例.cpp
+++ ComputationalGeomatry/最小覆盖圆实例.cpp
@@ -1,42 +1,58 @@
+/*
+ *  Source : HDU - 3932
+ *  Author : ConanYu
+ *  Complexity : O(n)
+ */
+
+
 #include<cstdio>
 #include<cmath>
 #include<algorithm>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
+
 struct point
 {
 	double x;
 	double y;
 	point(double _x=0.0,double _y=0.0):x(_x),y(_y){}
 }p[1005],O;
+
 double R;
+
 const double eps=1e-7;
+
 double sqr(const double & x)
 {
 	return x*x;
 }
+
 double dis(const point & x,const point & y)
 {
 	return sqrt(sqr(x.x-y.x)+sqr(x.y-y.y));
 }
+
 bool isin(const point & x)
 {
 	if(dis(O,x)<=R+eps)return true;
 	return false;
-}
+}// 是否在圆内
+
 point midd(const point & x,const point & y)
 {
 	double a=(x.x+y.x)/2.0;
 	double b=(x.y+y.y)/2.0;
 	return point(a,b);
-}
+}// 求中点坐标
+
 struct line
 {
 	point a,b;
 	line(){}
 	line(point _a,point _b):a(_a),b(_b){}
-};
+};// 两点式直线类
+
 point cross(const line & u,const line & v)
 {
 	point ret=u.a;
@@ -47,7 +63,8 @@ point cross(const line & u,const line & v)
 	ret.x+=(u.b.x-u.a.x)*t;
 	ret.y+=(u.b.y-u.a.y)*t;
 	return ret;
-}
+}// 求交点
+
 point cir(const point & a,const point & b,const point & c)
 {
 	line u,v;
@@ -60,7 +77,8 @@ point cir(const point & a,const point & b,const point & c)
 	v.b.x=v.a.x-a.y+c.y;
 	v.b.y=v.a.y+a.x-c.x;
 	return cross(u,v);
-}
+}// 求三个点的外心
+
 int main()
 {
 	srand(time(0));
@@ -68,9 +86,9 @@ int main()
 	while(~scanf("%d %d %d",&X,&Y,&n))
 	{
 		for(int i=0;i<n;i++)scanf("%lf %lf",&p[i].x,&p[i].y);
-		random_shuffle(p,p+n);
+		random_shuffle(p,p+n); // 打乱序列
 		O.x=O.y=R=0.0;
-        for(int i=0;i<n;i++)
+		for(int i=0;i<n;i++)
 		{
 			if(!isin(p[i]))
 			{
