5a48c0a6ee7db2a524d8a359c33c421062bf3528
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-05-03 22:57:52 +0800

    Update accelerateIOinJava.java

--- accelerateIO/accelerateIOinJava.java
+++ accelerateIO/accelerateIOinJava.java
@@ -4,60 +4,64 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-    	InputStream inputStream = System.in;
-    	OutputStream outputStream = System.out;
-    	InputReader in = new InputReader(inputStream);
-    	PrintWriter out = new PrintWriter(outputStream);
-    	Task solver = new Task();
-    	solver.solve(1, in, out);
-    	out.close();
-	}
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
 
-	static class Task {
-		public void solve(int testNumber, InputReader in, PrintWriter out) {
-			try {
-				
-			}
-			catch(RuntimeException e) {
-				return;
-			}
-		}
-	}
-    
-	static class InputReader {
-		public BufferedReader reader;
-		public StringTokenizer tokenizer;
-		public InputReader(InputStream stream) {
-			reader = new BufferedReader(new InputStreamReader(stream), 32768);
-			tokenizer = null;
-		}
+    static class Task {
+        public void solve(InputReader in, PrintWriter out) {
+            try {
 
-		public String next() {
-			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-				try {
-					tokenizer = new StringTokenizer(reader.readLine());
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			return tokenizer.nextToken();
-		}
+            } catch(RuntimeException e) {
+                return;
+            }
+        }
+    }
 
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
 
-		public long nextLong() {
-			return Long.parseLong(next());
-		}
-        
-		public BigInteger nextBigInteger() {
-			return new BigInteger(next());
-		}
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public BigInteger nextBigInteger() {
+            return new BigInteger(next());
+        }
+
+        public BigDecimal nextBigDecimal(){
+            return new BigDecimal(next());
+        }
     }
 }
