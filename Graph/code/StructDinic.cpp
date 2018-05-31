5696b688392f70109efb4bde1d3c51350d8e8f59
Seee <Semonchan666@gmail.com>
2018-05-31 23:12:26 +0800

    Update StructDinic.cpp

--- Graph/code/StructDinic.cpp
+++ Graph/code/StructDinic.cpp
@@ -6,14 +6,14 @@ using namespace std;
 template <typename T> struct Dinic {
 	int N, cnt;
 	int st, ed;
-	T Ans;
+	T Ans, temp;
 
-	static const int inf = 0x3f3f3f3f;
+	static const ll inf = 0x3f3f3f3f3f3f3f3f;
 
 	struct edge {
 		int v, next;
 		T w;
-		edge(int vv = 0, T ww = 0, int nn = -1) {
+		edge(const int& vv = 0, const T& ww = 0, const int& nn = -1) {
 			v = vv, w = ww, next = nn;
 		}
 	};
@@ -21,18 +21,18 @@ template <typename T> struct Dinic {
 	vector<edge> e;
 	vector<int> head, level, cur;
 
-	Dinic(int n = 0, int eg = 0, int s = 1, int t = 0x3f3f3f3f) : e(eg + 5), head(n + 5), level(n + 5) {
+	Dinic(const int& n = 0, const int& eg = 0, const int& s = 1, const int& t = 0x3f3f3f3f) : e(eg + 5), head(n + 5), level(n + 5) {
 		N = n, st = s, ed = min(n, t);
 		init();
 	}
 
-	void init()
+	inline void init()
 	{
 		cnt = 0;
 		fill(head.begin(), head.end(), -1);
 	}
 
-	void addedge(int u, int v, int w) 
+	inline void addedge(const int& u, const int& v, const T& w)
 	{
 		e[cnt] = edge(v, w, head[u]); head[u] = cnt++;
 		e[cnt] = edge(u, 0, head[v]); head[v] = cnt++;
@@ -56,16 +56,16 @@ template <typename T> struct Dinic {
 				}
 			}
 		}
-		
-		return level[ed] != 0;
+		return level[ed];
 	}
 
-	T Dfs(T flow, int u)
+	T Dfs(T flow, const int& u)
 	{
 		if (u == ed || flow == 0) return flow;
 
 		T delta = 0;
-		for (int& i = cur[u]; i != -1 && flow > 0; i = e[i].next) {
+		//for (int& i = cur[u]; i != -1 && flow > 0; i = e[i].next) {
+		for (int i = head[u]; i != -1 && flow > 0; i = e[i].next) {
 			int v = e[i].v;
 			if (level[v] == level[u] + 1 && e[i].w > 0) {
 				T tmp = Dfs(min(e[i].w, flow), v);
@@ -76,7 +76,7 @@ template <typename T> struct Dinic {
 				flow -= tmp;
 			}
 		}
-
+		if (delta == 0) level[u] = -1;
 		return delta;
 	}
 
@@ -84,8 +84,10 @@ template <typename T> struct Dinic {
 	{
 		Ans = 0;
 		while (Bfs()) {
-			cur = head;
-			Ans += Dfs(inf, st);
+			//cur = head;
+			while ((temp = Dfs(inf, st)) > 0) {
+				Ans += temp;
+			}
 		}
 		return Ans;
 	}
