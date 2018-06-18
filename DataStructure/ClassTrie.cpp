754fd96cbcc0a8e1f2a7f4405b9fbd41773b2b2b
Seee <Semonchan666@gmail.com>
2018-06-18 22:42:51 +0800

    换成更新的Seee定制版= =

--- DataStructure/ClassTrie.cpp
+++ DataStructure/ClassTrie.cpp
@@ -1,58 +1,64 @@
 #include <algorithm>
-#include <vector>
 #include <string.h>
-using namespace std;
+#include <vector>
+using std::vector;
 
-class Trie{
-public:
-	void init(int k_ = 1) // 初始化， k_为预估需求空间
-	{
-		cnt = 0;
-		v.resize(k_);
-		v[0] = node();
-	}
-	
-	void put(char t[], int len) // 把t[]放入Trie， len为t[]长度
-	{
-		int now = 0;
-		for (int i = 0; i < len; i++)
-		{
-			if (v[now].next[t[i] - 'a'] == 0)
-			{
-				v[now].next[t[i] - 'a'] = ++cnt;
-				if (cnt == v.size())
-					v.push_back(node());
-				else
-					v[cnt] = node();
-			}
-			now = v[now].next[t[i] - 'a'];
-		}
-		v[now].ex = 1;
-		v[now].cnt++;
-	}
-	
-	int check(char t[], int len) // 查找t[], 找到则返回以其为前缀的单词数量， 否则返回-1
-	{
-		int now = 0;
-		for (int i = 0; i < len; i++)
-		{
-			if (v[now].next[t[i] - 'a'] == 0)
-				return -1;
-			now = v[now].next[t[i] - 'a'];
-		}
-		return v[now].ex == 1 ? v[now].cnt : -1;
-	}
+/*
+usage:
 
-	Trie(){}
-	
-private:
-	struct node{
-		int ex, cnt;
-		int next[30];
-		node() { ex = 0; cnt = 0; memset(next, 0, sizeof next);	}
-	};
-	
-	int cnt;
-	vector<node> v;
+    Define: Trie T(MaxSize);
+
+    Initial: T.init();
+
+    Push in: T.push(Str, StrLen);
+
+    Check: T.check(Str, StrLen);
+*/
+
+struct Trie {
+
+    struct Node {
+        bool is_end;
+        int next[30];
+        Node() { is_end = 0; memset(next, 0, sizeof next); }
+    };
+
+    int cnt;
+    vector<Node> Vn;
+
+    // Max Size!!!!!
+    Trie(int Size = 0) : Vn(Size + 5) {
+        cnt = 0;
+    }
+
+    void init()
+    {
+        cnt = 0;
+        Vn[0] = Node();
+    }
+
+    void push(const char *arr, const int& len)
+    {
+        int now = 0;
+        for (int i = 0; i < len; i++) {
+            if (Vn[now].next[arr[i] - 'a'] == 0) {
+                Vn[++cnt] = Node();
+                Vn[now].next[arr[i] - 'a'] = cnt;
+            }
+            now = Vn[now].next[arr[i] - 'a'];
+        }
+        Vn[now].is_end = true;
+    }
+
+    bool check(const char *arr, const int& len)
+    {
+        int now = 0;
+        for (int i = 0; i < len; i++) {
+        if (Vn[now].next[arr[i] - 'a'] == 0) {
+                return false;
+            }
+            now = Vn[now].next[arr[i] - 'a'];
+        }
+        return Vn[now].is_end;
+    }
 };
-
