48d15e1c309325cfb700276ed3bc381123a0d7e2
Seee <Semonchan666@gmail.com>
2018-07-02 19:54:47 +0800

    Update FenwickTree.cpp

--- DataStructure/FenwickTree.cpp
+++ DataStructure/FenwickTree.cpp
@@ -3,27 +3,27 @@
 #include <vector>
 using std::vector;
 
-template <typename T> struct Fenwick{
+template <typename T> struct Fenwick {
 	int n;
 	vector<T> sum_;
-    
-	#define lowbit(x) (x & (-x))
-		
+
+#define lowbit(x) (x & (-x))
+
 	Fenwick(int n_ = 0) : n(n_), sum_(n + 5, 0) {}
-    
+
 	void upd(int i, T delta)
 	{
-        for (; i <= n; i += lowbit(i)) sum_[i] += delta;
+		for (; i <= n; i += lowbit(i)) sum_[i] += delta;
 	}
 	T getsum(int i)
-    {
+	{
 		T res = 0;
-        for (; i > 0; i -= lowbit(i)) res += sum_[i];
+		for (; i > 0; i -= lowbit(i)) res += sum_[i];
 		return res;
 	}
-    
+
 	T query(int l, int r)
-    {
+	{
 		return getsum(r) - getsum(l - 1);
-    }
+	}
 };
