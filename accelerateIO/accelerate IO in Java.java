99177de3ea5d99ac9191a26b048d9f60612ab8f3
Seee <Semonchan666@gmail.com>
2018-07-23 13:11:16 +0800

    UPD

--- accelerateIO/accelerate IO in Java.java
+++ accelerateIO/accelerate IO in Java.java
@@ -1,72 +1,26 @@
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.math.*;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-    	InputStream inputStream = System.in;
-    	OutputStream outputStream = System.out;
-    	InputReader in = new InputReader(inputStream);
-    	PrintWriter out = new PrintWriter(outputStream);
-    	Task solver = new Task();
-    	solver.solve(1, in, out);
-    	out.close();
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedInputStream(System.in));
+		PrintWriter out = new PrintWriter(Sysytem.out);
+		Task solver = new Task();
+		try {
+			solver.solve(in, out);
+		} catch (NoSuchElementException e) {
+			out.close();
+		}
 	}
-
-	static class Task {
-		public void solve(int testNumber, InputReader in, PrintWriter out) {
-			try {
-				
-			}catch(RuntimeException e) {
-				return;
-			}
+	public static class Task {
+		public static void solve(Scanner in, PrintWriter out) {
+			// Your code start here.
 			
 		}
 	}
-    
-	static class InputReader {
-		public BufferedReader reader;
-		public StringTokenizer tokenizer;
-		public InputReader(InputStream stream) {
-			reader = new BufferedReader(new InputStreamReader(stream), 32768);
-			tokenizer = null;
-		}
-
-		public String next() {
-			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-				try {
-					tokenizer = new StringTokenizer(reader.readLine());
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			return tokenizer.nextToken();
-		}
-
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		public long nextLong() {
-			return Long.parseLong(next());
-		}
-        
-		public BigInteger nextBigInteger() {
-			return new BigInteger(next());
-		}
-		
-		public BigDecimal nextBigDecimal() {
-			return new BigDecimal(next());
-		}
-		
-		public double nextDouble() {
-			return Double.parseDouble(next());
-		}
-    }
 }
