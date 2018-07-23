9a0d58e3bbc2e59217f836fe4f26c432b92bfdfc
Seee <Semonchan666@gmail.com>
2018-07-23 13:27:09 +0800

    UPD

--- accelerateIO/NewIO.cpp
+++ accelerateIO/NewIO.cpp
@@ -7,7 +7,7 @@ namespace io {
         static char buf[BUFLEN], *st = nullptr, *ed = nullptr;
         return (st == ed) ? ((ed = (st = buf) + fread(buf, 1, BUFLEN, stdin)), ((st == ed) ? -1 : *st++)) : *st++; 
     }
-    inline bool check(char& x) { return x == '-' || x == '\n' || x == ' ' || x == '\r' || x == '\t'; }
+    inline bool check(char x) { return x == '-' || x == '\n' || x == ' ' || x == '\r' || x == '\t'; }
     template <class I> inline void read(I& x) {
         char c; int f = 1;
         while (check(c = gc())) if (c == '-') f = -1;
@@ -15,7 +15,7 @@ namespace io {
         for (x = c - '0'; (c = gc()) >= '0' && c <= '9'; x = x * 10 + (c & 15)); x *= f;
     }
 #define Isalpha(a) ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-    inline void gstr(char *s, int& len) {
+    inline void gstr(char *s, int len) {
         char c;
         for (c = gc(); Isalpha(c); c = gc());
         if (c == -1) return;
@@ -24,7 +24,7 @@ namespace io {
     }
     char obuf[BUFLEN], *ost = obuf, *oed = obuf + BUFLEN - 1, Stack[55], Top;
     inline void flush() { fwrite(obuf, 1, ost - obuf, stdout); ost = obuf; }
-    inline void pc(char& x) { *ost++ = x; if (ost == oed) flush(); }
+    inline void pc(char x) { *ost++ = x; if (ost == oed) flush(); }
     template <class I> inline void print(I x) {
         if (!x) pc('0');
         if (x < 0) pc('-'), x = -x;
@@ -36,12 +36,4 @@ namespace io {
     struct IOFLUSHER { ~IOFLUSHER() { flush(); } } _ioflusher_;
 #undef BUFLEN
 }
-using io::EOFError;
-using io::flush;
-using io::gc;
-using io::gstr;
-using io::read;
-using io::pc;
-using io::print;
-using io::println;
-using io::pstr;
+using namespace io;
