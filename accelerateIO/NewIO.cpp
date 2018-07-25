5ef7bd9a7b3b2e5edbbe981197a0768b4d91642c
Seee <Semonchan666@gmail.com>
2018-07-25 22:54:05 +0800

    UPD

--- accelerateIO/NewIO.cpp
+++ accelerateIO/NewIO.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 namespace io {
-#define BUFLEN (1 << 21)
+    const int BUFLEN = (1 << 21) + 1;
     bool EOFError;
     inline char gc() {
         static char buf[BUFLEN], *st = nullptr, *ed = nullptr;
@@ -14,13 +14,10 @@ namespace io {
         if (c == -1)  { EOFError = 1; return; }
         for (x = c - '0'; (c = gc()) >= '0' && c <= '9'; x = x * 10 + (c & 15)); x *= f;
     }
-#define Isalpha(a) ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
     inline void gstr(char *s, int len) {
-        char c;
-        for (c = gc(); Isalpha(c); c = gc());
+        char c; for (c = gc(); c < 'a' || c > 'z'; c = gc());
         if (c == -1) return;
-        for (len = 0; Isalpha(c); c = gc()) s[len++] = c;
-        s[len] = 0;
+        for (len = 0; c >= 'a' && c <= 'z'; c = gc()) s[len++] = c; s[len] = 0;
     }
     char obuf[BUFLEN], *ost = obuf, *oed = obuf + BUFLEN - 1, Stack[55], Top;
     inline void flush() { fwrite(obuf, 1, ost - obuf, stdout); ost = obuf; }
@@ -34,6 +31,5 @@ namespace io {
     template <class I> inline void println(I x) { print(x), pc('\n'); }
     inline void pstr(char *s) { for (int i = 0, n = strlen(s); i < n; i++) pc(s[i]); }
     struct IOFLUSHER { ~IOFLUSHER() { flush(); } } _ioflusher_;
-#undef BUFLEN
 }
 using namespace io;
