79a88192976a6feb36050efd6eadc1915c0bc560
ConanYu <c.zing@outlook.com>
2018-08-08 13:57:47 +0800

    Java

--- accelerateIO/accelerate IO in Java.java
+++ accelerateIO/accelerate IO in Java.java
@@ -1,26 +1,24 @@
 import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.math.*;
 import java.util.NoSuchElementException;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(new BufferedInputStream(System.in));
-		PrintWriter out = new PrintWriter(Sysytem.out);
-		Task solver = new Task();
-		try {
-			solver.solve(in, out);
-		} catch (NoSuchElementException e) {
-			out.close();
-		}
-	}
-	public static class Task {
-		public static void solve(Scanner in, PrintWriter out) {
-			// Your code start here.
-			
-		}
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        Task solver = new Task();
+        try {
+            solver.solve(in, out);
+        } catch (NoSuchElementException e) {
+            out.close();
+        }
+    }
+    private static BigInteger[] a = new BigInteger[1005];
+    public static class Task {
+        public static void solve(Scanner in, PrintWriter out) {
+            // Your code start here.
+            
+        }
+    }
 }
