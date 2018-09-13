28295f7cdc52b6a135ee6b325a61ff6cd7fd500e
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-09-13 20:25:25 +0800

    Update factorization.py

--- NumberTheory/code/factorization.py
+++ NumberTheory/code/factorization.py
@@ -7,55 +7,49 @@ def gcd(x, y):
 
 
 def fpow(a, x, n):
-	ans = 1
-	while x > 0:
-		if x & 1:
-			ans = ans * a % n
-		a = a * a % n
-		x >>= 1
-	return ans
+    ans = 1
+    while x > 0:
+        if x & 1:
+            ans = ans * a % n
+        a = a * a % n
+        x >>= 1
+    return ans
 
 
 def check(a, n, x, t):
-	ret = fpow(a, x, n)
-	last = ret
-	for i in range(0, t):
-		ret = ret * ret % n
-		if ret == 1 and last != 1 and last != n - 1:
-			return True
-		last = ret
-	if ret != 1:
-		return True
-	return False
+    ret = fpow(a, x, n)
+    last = ret
+    for i in range(0, t):
+        ret = ret * ret % n
+        if ret == 1 and last != 1 and last != n - 1:
+            return True
+        last = ret
+    if ret != 1:
+        return True
+    return False
 
 
 # there change the times of Rabin-Miller
-TIMES = 100
+TIMES = 50
 
 
 def is_prime(n):
-	if n == 0 or n == 1:
-		return False
-	if n <= (1 << 20):
-		i = 2
-		while i * i <= n:
-			if n % i == 0:
-				return False
-			i += 1
-		return True
-	else:
-		if ~n & 1:
-			return False
-		x = n - 1
-		t = 0
-		while not x & 1:
-			x >>= 1
-			t += 1
-		for i in range(0, TIMES):
-			a = random.randint(1, n - 2)
-			if check(a, n, x, t):
-				return False
-		return True
+    if not isinstance(n, int):
+        raise TypeError(str(n) + ' is not a integer!')
+    if n in {2, 3, 5, 7, 11}:
+        return True
+    elif n == 1 or n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0 or n % 11 == 0:
+        return False
+    x = n - 1
+    t = 0
+    while not x & 1:
+        x >>= 1
+        t += 1
+    for i in range(0, TIMES):
+        a = random.randint(1, n - 2)
+        if check(a, n, x, t):
+            return False
+    return True
 
 
 def pollard_rho_2(n, c):
@@ -76,9 +70,9 @@ def pollard_rho_2(n, c):
 
 def pollard_rho_1(n, c):
     if not isinstance(n, int):
-        raise TypeError
+        raise TypeError(str(n) + ' is not a integer!')
     if n <= 0:
-        raise ValueError
+        raise ValueError('%d <= 0!' % n)
     if n == 1:
         return None
     if is_prime(n):
