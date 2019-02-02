01a9d7345b2a63f045f39eb6639c803cd135a7a0
ConanYu <33613505+ConanYu@users.noreply.github.com>
2019-02-02 15:39:14 +0800

    Create 莫比乌斯函数打表.cpp

---
+++ NumberTheory/code/莫比乌斯函数打表.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e7;
+bool isnpri[MAXN];
+vector<int>pri;
+short mu[MAXN];
+void mobius() {
+    mu[1] = 1;
+    for(int i = 2; i < MAXN; i++) {
+        if(!isnpri[i]) {
+            pri.push_back(i);
+            mu[i] = -1;
+        }
+        for(int j = 0; j < int(pri.size()); j++) {
+            const int cur = i * pri[j];
+            if(cur >= MAXN) {
+                break;
+            }
+            isnpri[cur] = true;
+            if(i % pri[j] == 0) {
+                mu[cur] = 0;
+                break;
+            } else {
+                mu[cur] = -mu[i];
+            }
+        }
+    }
+}
+
+int main() {
+    clock_t start = clock();
+    mobius();
+    cout << ((clock() - start) * 1.0 / CLOCKS_PER_SEC) << endl;
+}
+
+// i7-7700hq maxn=1e7 time=0.182s
