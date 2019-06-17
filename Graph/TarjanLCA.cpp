4ca3e37bd91d2956a0e4398357983ba574f3fefd
SemonChan <Semonchan666@gmail.com>
2019-06-17 16:38:11 +0800

    upd

--- Graph/TarjanLCA.cpp
+++ Graph/TarjanLCA.cpp
@@ -1,104 +1,31 @@
-#include <algorithm>
-#include <vector>
-using namespace std;
+const int N = 1e5 + 5;
+const int M = 1e5 + 5;
 
-struct Dsu {
-	int N, cnt;
-	vector<int> Root;
+vector<int> G[N];
+vector<pair<int, int>> Q[N];
+int lca[M], anc[N];
+bool vis[N];
 
-	Dsu(int num = 0) : N(num), Root(num + 5), cnt(num) {
-		for (int i = 0; i < num; i++) Root[i] = i;
-	}
+void init(int n) {
+  iota(anc, anc + n, 0);
+}
 
-	void init() // Initial
-	{
-		cnt = N;
-		for (int i = 0; i < N; i++) Root[i] = i;
-	}
+void addEdge(int u, int v);
 
-	int find(int x) // Get Ancestor
-	{
-		if (x == Root[x]) return x;
-		else return Root[x] = find(Root[x]);
-	}
+void addQuery(int u, int v, int i) {
+  Q[u].push_back({ v, i }), Q[v].push_back({ u, i });
+}
 
-	bool Union(int a, int b)
-	{
-		if (!same(a, b)) {
-			cnt--; // Trees in forest
-			Root[Root[b]] = Root[a]; // Link b to a
-			return true;
-		}
-		return false;
-	}
+int find(int x) {
+  return x == anc[x] ? x : anc[x] = find(anc[x]);
+}
 
-	bool same(int a, int b)
-	{
-		a = find(a);
-		b = find(b);
-		return a == b; // Same Ancestor?
-	}
-};
-
-struct TarjanLCA {
-	int N;
-	Dsu D;
-	vector<vector<int>> e;
-	vector<int> Color;
-
-	struct Node {
-		int u, v, w;
-		Node(int uu = 0, int vv = 0, int ww = 0) {
-			u = uu, v = vv, w = ww;
-		}
-	};
-
-	vector<vector<Node>> Q;
-	vector<Node> Ans;
-
-	TarjanLCA(int n = 0) : N(n + 5), D(n + 5), \
-		e(n + 5), Color(n + 5), Q(n + 5) { init(); }
-
-	void init(int n = -1) // Initial
-	{
-		if (n < 0) n = N - 1;
-		for (int i = 1; i <= n; i++) e[i].clear(), Q[i].clear();
-		fill(Color.begin(), Color.end(), 0);
-		Ans.clear();
-		D.init();
-	}
-
-	void addedge(const int& u, const int& v)
-	{
-		e[u].push_back(v);
-		e[v].push_back(u);
-	}
-
-	void addquery(const int& u, const int& v)
-	{
-		Q[u].emplace_back(u, v, Ans.size());
-		Q[v].emplace_back(v, u, Ans.size());
-		Ans.emplace_back(u, v, -1);
-	}
-
-	void Dfs(const int& u, const int& pre)
-	{
-		for (int i = 0; i < e[u].size(); i++) {
-			if (Color[e[u][i]] || e[u][i] == pre) continue;
-			Dfs(e[u][i], u); // Dfs child
-		}
-
-		for (int i = 0; i < Q[u].size(); i++) {
-			if (!Color[Q[u][i].v]) continue;
-			Ans[Q[u][i].w].w = D.find(Q[u][i].v); // Get LCA
-		}
-
-		Color[u] = 1;
-		if (pre != -1) D.Union(pre, u); // Attention!! Link u to pre!!!
-	}
-
-	void Solve()
-	{
-		Dfs(1, -1); // Consider 1 as the highest ancestor
-	}
-};
+void dfs(int u, int f) {
+  for (auto& v : G[u]) {
+    if (v != f) dfs(v, u), vis[v] = 1, anc[v] = anc[u];
+  }
+  for (auto& q : Q[u]) {
+    int v = q.first, i = q.second;
+    if (vis[v]) lca[i] = find(v);
+  }
+}
