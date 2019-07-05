285f130b43d8666648629cd7ee7de9e8e62ce82f
余梓宁 <33613505+ConanYu@users.noreply.github.com>
2019-07-05 22:25:00 +0800

    upd sa with sais

--- String/SuffixArray.cpp
+++ String/SuffixArray.cpp
@@ -48,79 +48,121 @@ namespace SuffixArray {
 
 
 // ****************************************************************************************
-// dc3 O(n)
+// sais O(n)
 // unfinished but usable
 
-namespace SA {
-  #define F(x) ((x)/3+((x)%3==1?0:tb))
-  #define G(x) ((x)<tb?(x)*3+1:((x)-tb)*3+2)
-  const int N = 2e4 + 5;
-  const int MAXN = N * 4; // maybe 3 times is enough?
-  int wv[N], wws[N], wa[N], wb[N], ssa[MAXN], r[MAXN];
-  void Sort(int *r, int *a, int *b, int n, int m) {
-    memset(wws, 0, sizeof(int) * m);
-    for(int i = 0; i < n; i++) wv[i] = r[a[i]];
-    for(int i = 0; i < n; i++) wws[wv[i]]++;
-    for(int i = 1; i < m; i++) wws[i] += wws[i - 1];
-    for(int i = n - 1; i >= 0; i--) b[--wws[wv[i]]] = a[i];
+const size_t sz = 3e5 + 5;
+int bucket[sz], bucket1[sz], sa[sz], rk[sz], ht[sz];bool type[sz << 1];
+
+struct SuffixArray {
+  bool isLMS(const int i, const bool *type) {
+    return i > 0 && type[i] && !type[i - 1];
   }
-  int c0(int *r, int a, int b) {
-    return r[a] == r[b] && r[a + 1] == r[b + 1] && r[a + 2] == r[b + 2];
-  }
-  int c12(int k, int *r, int a, int b) {
-    if(k == 2) return r[a] < r[b] || r[a] == r[b] && c12(1, r, a + 1, b + 1);
-    return r[a] < r[b] || r[a] == r[b] && wv[a + 1] < wv[b + 1];
-  }
-  void dc3(int *r, int *sa, int n, int m) {
-    n++, m++;
-    int i, j, *rn = r + n, *san = sa + n, ta = 0, tb = (n + 1) / 3, tbc = 0, p;
-    r[n] = r[n + 1] = 0;
-    for(i = 0; i < n; i++) if(i % 3 != 0) wa[tbc++] = i;
-    Sort(r + 2, wa, wb, tbc, m);
-    Sort(r + 1, wb, wa, tbc, m);
-    Sort(r, wa, wb, tbc, m);
-    for(p = 1, rn[F(wb[0])] = 0, i = 1; i < tbc; i++) rn[F(wb[i])] = c0(r, wb[i - 1], wb[i]) ? p - 1 : p++;
-    if(p < tbc) dc3(rn, san, tbc - 1, p - 1);
-    else for(i = 0; i < tbc; i++) san[rn[i]] = i;
-    for(i = 0; i < tbc; i++) if(san[i] < tb) wb[ta++] = san[i] * 3;
-    if(n % 3 == 1) wb[ta++] = n - 1;
-    Sort(r, wb, wa, ta, m);
-    for(i = 0; i < tbc; i++) wv[wb[i] = G(san[i])] = i;
-    for(i = 0, j = 0, p = 0; i < ta && j < tbc; p++) sa[p] = c12(wb[j] % 3, r, wa[i], wb[j]) ? wa[i++] : wb[j++];
-    for(; i < ta; p++) sa[p] = wa[i++];
-    for(; j < tbc; p++) sa[p] = wb[j++];
-  }
-  // below for whom don't know how to use the above
-  template<typename T>
-  vector<int> SA(const T &s) {
-    const int n = s.size();
-    int mx = 0;
-    for(int i = 0; i < n; i++) {
-      r[i] = int(s[i]) + 1;
-      mx = max(r[i], mx);
+  template<class T>
+  void inducedSort(T s, int *sa, const int len, const int sigma, const int bucketSize, bool *type, int *bucket, int *cntbuf, int *p) {
+    memset(bucket, 0, sizeof(int) * sigma);
+    memset(sa, -1, sizeof(int) * len);
+    for (int i = 0; i < len; i++) {
+      bucket[s[i]]++;
     }
-    dc3(r, ssa, n, mx);
-    vector<int> ret(n + 1);
-    for(int i = 0; i <= n; i++) {
-      ret[i] = ssa[i];
+    cntbuf[0] = bucket[0];
+    for (int i = 1; i < sigma; i++) {
+      cntbuf[i] = cntbuf[i - 1] + bucket[i];
     }
-    return ret;
-  }
-  vector<int> RANK(const vector<int> &sa) {
-    vector<int> ret(sa.size() - 1);
-    for(int i = 1; i < int(sa.size()); i++) {
-      ret[sa[i]] = i;
+    for (int i = bucketSize - 1; i >= 0; i--) {
+      sa[--cntbuf[s[p[i]]]] = p[i];
     }
-    return ret;
+    for (int i = 1; i < sigma; i++) {
+      cntbuf[i] = cntbuf[i - 1] + bucket[i - 1];
+    }
+    for (int i = 0; i < len; i++) {
+      if (sa[i] > 0 && !type[sa[i] - 1]) {
+        sa[cntbuf[s[sa[i] - 1]]++] = sa[i] - 1;
+      }
+    }
+    cntbuf[0] = bucket[0];
+    for (int i = 1; i < sigma; i++) {
+      cntbuf[i] = cntbuf[i - 1] + bucket[i];
+    }
+    for (int i = len - 1; i >= 0; i--) {
+      if (sa[i] > 0 && type[sa[i] - 1]) {
+        sa[--cntbuf[s[sa[i] - 1]]] = sa[i] - 1;
+      }
+    }
   }
   template<typename T>
-  vector<int> HEIGHT(const T &s, const vector<int> &sa, const vector<int> &rank) {
-    const int n = int(rank.size());
-    int k = 0; vector<int> ret(n + 1);
-    for(int i = 0; i < n; ret[rank[i++]] = k) {
-      if(k) k--;
-      for(int j = sa[rank[i] - 1]; s[i + k] == s[j + k]; k++);
+  void sais(T s, int *sa, int len, bool *type, int *bucket, int *bucket1, int sigma) {
+    int i, j, bucketSize = 0, cnt = 0, p = -1, x, *cntbuf = bucket + sigma;
+    type[len - 1] = 1;
+    for (i = len - 2; i >= 0; i--) {
+      type[i] = s[i] < s[i + 1] || (s[i] == s[i + 1] && type[i + 1]);
     }
-    return ret;
+    for (i = 1; i < len; i++) {
+      if (type[i] && !type[i - 1]) {
+        bucket1[bucketSize++] = i;
+      }
+    }
+    inducedSort(s, sa, len, sigma, bucketSize, type, bucket, cntbuf, bucket1);
+    for (i = bucketSize = 0; i < len; i++) {
+      if (isLMS(sa[i], type)) {
+        sa[bucketSize++] = sa[i];
+      }
+    }
+    for (i = bucketSize; i < len; i++) {
+      sa[i] = -1;
+    }
+    for (i = 0; i < bucketSize; i++) {
+      x = sa[i];
+      for (j = 0; j < len; j++) {
+        if (p == -1 || s[x + j] != s[p + j] || type[x + j] != type[p + j]) {
+          cnt++, p = x;
+          break;
+        } else if (j > 0 && (isLMS(x + j, type) || isLMS(p + j, type))) {
+          break;
+        }
+      }
+      x = (~x & 1 ? x >> 1 : x - 1 >> 1), sa[bucketSize + x] = cnt - 1;
+    }
+    for (i = j = len - 1; i >= bucketSize; i--) {
+      if (sa[i] >= 0) {
+        sa[j--] = sa[i];
+      }
+    }
+    int *s1 = sa + len - bucketSize, *bucket2 = bucket1 + bucketSize;
+    if (cnt < bucketSize) {
+      sais(s1, sa, bucketSize, type + len, bucket, bucket1 + bucketSize, cnt);
+    } else {
+      for (i = 0; i < bucketSize; i++) {
+        sa[s1[i]] = i;
+      }
+    }
+    for (i = 0; i < bucketSize; i++) {
+      bucket2[i] = bucket1[sa[i]];
+    }
+    inducedSort(s, sa, len, sigma, bucketSize, type, bucket, cntbuf, bucket2);
+  }
+  void getHeight(const char *s, const int len, const int *sa) {
+    for (int i = 0, k = 0; i < len; i++) {
+      if (rk[i] == 0) {
+        k = 0;
+      } else {
+        if (k > 0) {
+          k--;
+        }
+        int j = sa[rk[i] - 1];
+        while (i + k < len && j + k < len && s[i + k] == s[j + k]) {
+          k++;
+        }
+      }
+      ht[rk[i]] = k;
+    }
+  }
+  template<class T>
+  void init(T s, const int len, const int sigma) {
+    sais(s, sa, len, type, bucket, bucket1, sigma);
+    for (int i = 1; i < len; i++) {
+      rk[sa[i]] = i;
+    }
+    getHeight(s, len, sa);
   }
 };
