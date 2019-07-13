671ba44bbdb58a050d79867ebdcc30e5a1ccea9d
ConanYu <c.zing@outlook.com>
2019-07-13 21:32:11 +0800

    upd poly with multipoint calc

--- NumberTheory/poly.cpp
+++ NumberTheory/poly.cpp
@@ -1,5 +1,5 @@
 // reference exgcd and fft::multiply_mod
-// make sure your nums are positive
+// make sure your nums are values in [0, MOD)
 namespace poly {
   const int MOD = 998244353ll;
   vector<int> inv(const vector<int> &a) {
@@ -21,8 +21,12 @@ namespace poly {
   }
   
   // A = B * C + D
+  // make sure A.size() >= B.size()
   vector<int> divide(const vector<int> &a, const vector<int> &b) {
     const int n = a.size(), m = b.size();
+    if(n < m) {
+      return {};
+    }
     vector<int> A(a), B(b);
     reverse(A.begin(), A.end()), reverse(B.begin(), B.end());
     A.resize(n - m + 1), B.resize(n - m + 1);
@@ -33,7 +37,8 @@ namespace poly {
   }
   
   vector<int> mod(const vector<int> &a, const vector<int> &b, const vector<int> &c) {
-    const int m = b.size();
+    const int n = a.size(), m = b.size();
+    if(n < m) return a;
     vector<int> e = fft::multiply_mod(b, c, MOD);
     e.resize(m - 1);
     for(int i = 0; i < m - 1; i++) {
@@ -43,5 +48,39 @@ namespace poly {
       }
     }
     return e;
+  }
+
+  vector<int> buildPoly(const vector<int> &vec, const int left, const int right) {
+    if(left == right) {
+      vector<int> ret;
+      ret.push_back(MOD - vec[left]);
+      ret.push_back(1);
+      return ret;
+    }
+    const int mid = (left + right) >> 1;
+    return fft::multiply_mod(buildPoly(vec, left, mid), buildPoly(vec, mid + 1, right), MOD);
+  }
+
+  void multipointCalc(const vector<int> &poly, const vector<int> &vec, const int left, const int right, vector<int> &ret) {
+    const int n = poly.size(), mid = (left + right) >> 1;
+    if(n == 1) {
+      for(int i = left; i <= right; i++) {
+        ret[i] = poly[0];
+      }
+      return;
+    }
+    const vector<int> b0 = buildPoly(vec, left, mid);
+    multipointCalc(mod(poly, b0, divide(poly, b0)), vec, left, mid, ret);
+    if(left != right) {
+      const vector<int> b1 = buildPoly(vec, mid + 1, right);
+      multipointCalc(mod(poly, b1, divide(poly, b1)), vec, mid + 1, right, ret);
+    }
+  }
+
+  vector<int> multipointCalc(const vector<int> &poly, const vector<int> &vec) {
+    const int n = vec.size();
+    vector<int> ret(n);
+    multipointCalc(poly, vec, 0, n - 1, ret);
+    return ret;
   }
 }
