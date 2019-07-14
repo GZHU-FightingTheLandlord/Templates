73c122416622ab5c55480bca785ff6915417cb6d
ConanYu <c.zing@outlook.com>
2019-07-14 12:35:53 +0800

    upd interpolate

--- NumberTheory/poly.cpp
+++ NumberTheory/poly.cpp
@@ -1,5 +1,7 @@
-// reference exgcd and fft::multiply_mod
-// make sure your nums are values in [0, MOD)
+// 引用 exgcd 和 fft::multiply_mod
+// 确保所有输入的数在[0, MOD)的区间中
+// MOD < 1073741823 以及 最好是质数
+// 传入的vector为{a_0, a_1, a_2, ..., a_n} 即认定为 y=\sum_{i=0}^{n}a_i\cdot x^i
 namespace poly {
   const int MOD = 998244353ll;
   vector<int> inv(const vector<int> &a) {
@@ -22,12 +24,10 @@ namespace poly {
   
   // A = B * C + D (mod x^n) (n = A.size())
   // always use with the next function mod
-  // make sure A.size() >= B.size() or else if will return an empty vector
+  // make sure A.size() >= B.size() or else it will return an empty vector
   vector<int> divide(const vector<int> &a, const vector<int> &b) {
     const int n = a.size(), m = b.size();
-    if(n < m) {
-      return {};
-    }
+    if(n < m) return {};
     vector<int> A(a), B(b);
     reverse(A.begin(), A.end()), reverse(B.begin(), B.end());
     A.resize(n - m + 1), B.resize(n - m + 1);
@@ -50,31 +50,6 @@ namespace poly {
     }
     return e;
   }
-
-  // 求导
-  vector<int> derivate(const vector<int> &vec) {
-    const int n = vec.size();
-    if(n <= 1) {
-      return {0};
-    }
-    vector<int> v(n - 1);
-    for(int i = 1; i < n; i++) {
-      v[i - 1] = 1ll * vec[i] * i % MOD;
-    }
-    return v;
-  }
-
-  // 积分
-  // vector<int> intergral(const vector<int> &vec) {
-
-  // }
-
-  // 开根号
-  // vector<int> sqrt(const vector<int> &vec) {
-    
-  // }
-
-  // 对数函数 指数函数 快速幂
 
   vector<int> buildPoly(const vector<int> &vec, const int left, const int right) {
     if(left == right) {
@@ -111,17 +86,48 @@ namespace poly {
     return ret;
   }
 
-  // 快速插值 <x, y>
-  // vector<int> interpolate(const vector<pair<int, int>> &p) {
-  //   const int n = p.size();
-  //   vector<int> x(n);
-  //   for(int i = 0; i < n; i++) {
-  //     x[i] = p[i].first;
-  //   }
-  //   vector<int> fm = buildPoly(x, 0, n - 1);
-  //   fm = derivate(fm);
-  //   fm = multipointCalc(fm, x);
-  //   // above is for 分母
-  //   // ...
-  // }
+  vector<int> multiInv(const vector<int> &vec) {
+    const int n = vec.size();
+    vector<int> a(n + 1), ret(n); a[0] = 1;
+    for(int i = 1; i <= n; i++) {
+      a[i] = 1ll * a[i - 1] * vec[i - 1] % MOD;
+    }
+    int cur = (exgcd(a[n], MOD).first + MOD) % MOD;
+    for(int i = n - 1; i >= 0; i--) {
+      ret[i] = 1ll * cur * a[i] % MOD;
+      cur = 1ll * cur * vec[i] % MOD;
+    }
+    return ret;
+  }
+
+  // 快速插值 {{x0, y0}, {x1, y1}, {x2, y2}, ...}
+  vector<int> interpolate(const vector<pair<int, int>> &p) {
+    const int n = p.size(), n0 = (n + 1) >> 1, n1 = n - n0;
+    if(n == 1) {
+      return {p[0].second};
+    }
+    vector<pair<int, int>> p0(p.begin() + n1, p.end());
+    vector<int> f0 = interpolate(p0);
+    vector<int> x(n);
+    for(int i = 0; i < n; i++) {
+      x[i] = p[i].first;
+    }
+    vector<int> g0 = buildPoly(x, n1, n - 1);
+    x.resize(n1);
+    vector<int> fx = multipointCalc(f0, x), gx = multipointCalc(g0, x);
+    gx = multiInv(gx);
+    p0.resize(n1);
+    for(int i = 0; i < n1; i++) {
+      p0[i].first = p[0].first;
+      p0[i].second = (p[i].second - fx[i] + MOD) % MOD;
+      p0[i].second = 1ll * p0[i].second * gx[i] % MOD;
+    }
+    fx = interpolate(p0);
+    fx = fft::multiply_mod(fx, g0, MOD);
+    fx.resize(n), f0.resize(n);
+    for(int i = 0; i < n; i++) {
+      fx[i] = (fx[i] + f0[i]) % MOD;
+    }
+    return fx;
+  }
 }
