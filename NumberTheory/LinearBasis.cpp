1ba544596a34b2c75558fec0b1bfddd1b282b8e5
ConanYu <c.zing@outlook.com>
2019-07-28 23:26:59 +0800

    upd lb unfinished

--- NumberTheory/LinearBasis.cpp
+++ NumberTheory/LinearBasis.cpp
@@ -1,34 +1,73 @@
-struct LinearBasis {
-  const static int MAXL = 50;
-  long long a[MAXL + 1];
-  LinearBasis() {
-    memset(a, 0, sizeof a);
-  }
-  void insert(long long t) {
-    for (int j = MAXL; j >= 0; j--) {
-      if (!(t & (1ll << j))) {
-        continue;
+typedef unsigned long long ull;
+struct LB {
+  const static int L = 64; // insert(x) 0 <= x < (1 << L)
+  ull a[L];
+  LB() { this->init(); }
+  void init() { memset(a, 0, sizeof a); }
+  ull &operator[](const size_t &id) { return a[id]; }
+  const ull &operator[](const size_t &id) const { return a[id]; }
+  bool insert(ull x) {
+    for(int i = L - 1; ~i; i--) {
+      if((x >> i) & 1) {
+        if(!a[i]) {
+          a[i] = x;
+          return true;
+        } else {
+          x ^= a[i];
+        }
       }
-      if (a[j]) {
-        t ^= a[j];
-      } else {
-        for (int k = 0; k < j; k++) {
-          if (t & (1ll << k)) {
-            t ^= a[k];
-          }
+      if(!x) {
+        return false;
+      }
+    }
+    return true;
+  }
+  // 高斯消元 化为行简化阶梯型
+  void operator()() {
+    for(int i = L - 1; ~i; i--) {
+      if(a[i]) {
+        for(int j = i + 1; j < L; j++) {
+          a[j] = min(a[j], a[j] ^ a[i]);
         }
-        for (int k = j + 1; k <= MAXL; k++) {
-          if (a[k] & (1ll << j)) {
-            a[k] ^= t;
-          }
-        }
-        a[j] = t;
-        return;
       }
     }
   }
+  // 交 （不能用 先交个作业？
+  friend LB operator&(const LB &A, const LB &B) {
+    LB C, D, E;
+    for(int i = L - 1; ~i; i--) {
+      if(A[i]) {
+        C.insert(A[i]);
+        D[i] = 1ull << i;
+      }
+    }
+    for(int i = 0; i < L; i++) {
+      if(!B[i]) {
+        continue;
+      }
+      ull v = 0, x = B[i];
+      for(int j = L - 1; ~j; j--) {
+        if((x >> j) & 1) {
+          if(C[j]) {
+            x ^= C[j], v ^= D[j];
+          } else {
+            C[j] = x, D[j] = v;
+            break;
+          }
+        }
+      }
+      if(!x) {
+        for(int j = L - 1; ~j; j--) {
+          if(v & 1) {
+            E.insert(C[j]);
+          }
+          v >>= 1;
+        }
+      }
+    }
+    return E;
+  }
 };
-
 
 // 线性基区间最大值
 
