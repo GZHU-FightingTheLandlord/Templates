0a88021bfc6a7afbe6443a6d626cbcbca6a43ea6
ConanYu <c.zing@outlook.com>
2019-09-07 19:29:05 +0800

    chg indent

--- NumberTheory/exgcd.cpp
+++ NumberTheory/exgcd.cpp
@@ -1,7 +1,5 @@
 pll exgcd(const long long x, const long long y) {
-    if (!y) {
-        return make_pair(1, 0);
-    }
-    pll cur = exgcd(y, x % y);
-    return make_pair(cur.second, cur.first - (x / y) * cur.second);
+  if (!y) return {1, 0};
+  pll cur = exgcd(y, x % y);
+  return {cur.second, cur.first - (x / y) * cur.second};
 }
