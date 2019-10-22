60e07f0f6eaffe761e061db2834deaaba8f297a1
ConanYu <c.zing@outlook.com>
2019-10-22 15:41:21 +0800

    upd miller_rabin and pollard_rho

--- NumberTheory/pollard_rho.cpp
+++ NumberTheory/pollard_rho.cpp
@@ -1,25 +1,32 @@
-int tot;
-long long factor[10000];
+typedef long long ll;
 
-long long pollard_rho(long long x, long long c) {
-    long long i = 1, k = 2;
-    long long x0 = rand() % x, y = x0;
-    while (true) {
-        i++;
-        x0 = (mul(x0, x0, x) + c) % x;
-        long long d = __gcd(y - x0, x);
-        if (d != 1 && d != x) return d;
-        if (y == x0) return x;
-        if (i == k) { y = x0, k <<= 1; }
-    }
+int tot;
+ll factor[10000];
+
+ll pollard_rho(ll x, ll c) {
+  ll i = 1, k = 2;
+  ll x0 = rand() % (x - 1) + 1, y = x0;
+  while (true) {
+    i++;
+    x0 = (mul(x0, x0, x) + c) % x;
+    ll d = __gcd(y - x0, x);
+    if(d < 0) d = -d;
+    if (d != 1 && d != x) return d;
+    if (y == x0) return x;
+    if (i == k) y = x0, k <<= 1;
+  }
 }
 
-void findfac(long long n) {
-    if (Miller(n)) {
-        factor[tot++] = n;
-        return;
-    }
-    long long p = n;
-    while (p >= n) p = pollard_rho(p, rand() % (n - 1) + 1);
-    findfac(p), findfac(n / p);
+void findfac(ll n, int k=107) {
+  if(n == 1) return;
+  if (isprime(n)) {
+    factor[tot++] = n;
+    return;
+  }
+  ll p = n;
+  int c = k;
+  while (p >= n) {
+    p = pollard_rho(p, c--);
+  }
+  findfac(p, k), findfac(n / p, k);
 }
