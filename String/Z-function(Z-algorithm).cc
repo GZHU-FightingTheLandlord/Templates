7cd05b02df3f16ca8996a84eab38dc93ef64e3f6
SemonChan <Semonchan666@gmail.com>
2020-03-09 22:10:22 +0800

    upd
    1、new version of topological sort.
    2、rename Z-function

--- String/Z-function(Z-algorithm).cc
+++ String/Z-function(Z-algorithm).cc
@@ -7,7 +7,7 @@ using namespace std;
 
 // 与exkmp类似
 // z[i]: suf_s[i..n-1] 与 s[0...n-1] 的公共前缀长度
-vector<int> getZ(string s) {
+vector<int> z_function(string s) {
   int n = (int)s.length();
   vector<int> z(n);
   for (int i = 1, l = 0, r = 0; i < n; i++) {
