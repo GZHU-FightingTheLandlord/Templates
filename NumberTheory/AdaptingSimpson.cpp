9542b461a7c537bc72870bbafe97ad145b0a95ca
ConanYu <c.zing@outlook.com>
2020-07-15 13:04:12 +0800

    feat: 新增更人性化的自适应辛普森积分

--- NumberTheory/AdaptingSimpson.cpp
+++ NumberTheory/AdaptingSimpson.cpp
@@ -7,22 +7,34 @@
     的积分
 */
 
-/* 
-   调用： asr(a, b, eps, simpson(a, b))
-   其中eps为自定义误差
-*/ 
-
-double simpson(const double& a, const double& b)
-{
+namespace AdaptingSimpson {
+  template<typename Functor>
+  double simpson(const double &a, const double &b, const Functor &functor) {
     double c = (a + b) / 2;
-    return (F(a) + 4 * F(c) + F(b)) * (b - a) / 6;
-}
-
-double asr(double a, double b, double eps, double A)
-{
+    return (functor(a) + 4 * functor(c) + functor(b)) * (b - a) / 6;
+  }
+  template<typename Functor>
+  double asr(double a, double b, double eps, double A, const Functor &functor) {
     double c = (a + b) / 2;
-    double L = simpson(a, c), R = simpson(c, b);
-    if (fabs(L + R - A) <= 15 * eps)
-        return L + R + (L + R - A) / 15.0;
-    return asr(a, c, eps / 2, L) + asr(c, b, eps / 2, R);
+    double L = simpson(a, c, functor), R = simpson(c, b, functor);
+    if (fabs(L + R - A) <= 15 * eps) {
+      return L + R + (L + R - A) / 15.0;
+    }
+    return asr(a, c, eps / 2, L, functor) + asr(c, b, eps / 2, R, functor);
+  }
+  template<typename Functor>
+  double integrate(double from, double to, const Functor &functor, double eps=1e-5) {
+    return asr(from, to, eps, simpson(from, to, functor), functor);
+  }
 }
+using AdaptingSimpson::integrate;
+
+/*
+使用举例：
+double f(double x) {
+  return sin(x);
+}
+int main() {
+  cout << fixed << setprecision(1) << integrate(0, 1, f) << '\n';
+}
+*/
