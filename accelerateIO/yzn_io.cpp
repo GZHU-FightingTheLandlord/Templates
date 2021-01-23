034e69f5ae1080bf736c2111a5779135500887b0
ConanYu <33613505+ConanYu@users.noreply.github.com>
2021-01-23 16:14:55 +0800

    refactor: Refactor

--- accelerateIO/yzn_io.cpp
+++ accelerateIO/yzn_io.cpp
@@ -1,4 +1,5 @@
 namespace io {
+namespace detail {
   const size_t buflen = (1 << 21) + 1;
   char buf[buflen], *st = nullptr, *ed = nullptr;
   inline char gc() { return ((st == ed) ? (st = buf, ed = st + fread(buf, 1, buflen, stdin), (st == ed) ? EOF : *st++) : *st++); }
@@ -22,58 +23,56 @@ namespace io {
   }
   template <typename T> inline void Prln(T x) { Pr(x); pc('\n'); }
   struct _IOflusher_ { ~_IOflusher_() { flush(); } } __flusher__;
-}
-
-#ifdef __SIZEOF_INT128__
-#define __INT128__
-#endif
-
+}  // namespace detail
 struct instream {
-  instream &operator>>(char &__n) { __n = io::gc(); return *this; }
-  instream &operator>>(unsigned char &__n) { __n = io::gc(); return *this; }
-  instream &operator>>(short &__n) { io::Re(__n); return *this; }
-  instream &operator>>(unsigned short &__n) { io::Re(__n); return *this; }
-  instream &operator>>(int &__n) { io::Re(__n); return *this; }
-  instream &operator>>(unsigned int &__n) { io::Re(__n); return *this; }
-  instream &operator>>(long &__n) { io::Re(__n); return *this; }
-  instream &operator>>(unsigned long &__n) { io::Re(__n); return *this; }
-  instream &operator>>(long long &__n) { io::Re(__n); return *this; }
-  instream &operator>>(unsigned long long &__n) { io::Re(__n); return *this; }
-#ifdef __INT128__
-  instream &operator>>(__int128 &__n) { io::Re(__n); return *this;}
-  instream &operator>>(__uint128_t &__n) { io::Re(__n); return *this; }
+  instream &operator>>(char &__n) { __n = detail::gc(); return *this; }
+  instream &operator>>(unsigned char &__n) { __n = detail::gc(); return *this; }
+  instream &operator>>(short &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(unsigned short &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(int &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(unsigned int &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(long &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(unsigned long &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(long long &__n) { detail::Re(__n); return *this; }
+  instream &operator>>(unsigned long long &__n) { detail::Re(__n); return *this; }
+#ifdef __SIZEOF_INT128__
+  instream &operator>>(__int128 &__n) { detail::Re(__n); return *this;}
+  instream &operator>>(__uint128_t &__n) { detail::Re(__n); return *this; }
 #endif
   instream &operator>>(std::string &__n) {
     __n.clear();
-    char c = io::gc();
+    char c = detail::gc();
     if(c == EOF) return *this;
-    while(c != EOF && io::blank(c)) c = io::gc();
+    while(c != EOF && detail::blank(c)) c = detail::gc();
     if(c == EOF) return *this;
-    while(c != EOF && !io::blank(c)) {
+    while(c != EOF && !detail::blank(c)) {
       __n.push_back(c);
-      c = io::gc();
+      c = detail::gc();
     }
-    if(c != EOF) io::st--;
+    if(c != EOF) detail::st--;
     return *this;
   }
 } in;
 struct outstream {
-  outstream &operator<<(const char &__n) { io::pc(__n); return *this; }
-  outstream &operator<<(const unsigned char &__n) { io::pc(__n); return *this; }
-  outstream &operator<<(const short &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const unsigned short &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const int &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const unsigned int &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const long &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const unsigned long &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const long long &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const unsigned long long &__n) { io::Pr(__n); return *this; }
-#ifdef __INT128__
-  outstream &operator<<(const __int128 &__n) { io::Pr(__n); return *this; }
-  outstream &operator<<(const __uint128_t &__n) { io::Pr(__n); return *this; }
+  outstream &operator<<(const char &__n) { detail::pc(__n); return *this; }
+  outstream &operator<<(const unsigned char &__n) { detail::pc(__n); return *this; }
+  outstream &operator<<(const short &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const unsigned short &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const int &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const unsigned int &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const long &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const unsigned long &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const long long &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const unsigned long long &__n) { detail::Pr(__n); return *this; }
+#ifdef __SIZEOF_INT128__
+  outstream &operator<<(const __int128 &__n) { detail::Pr(__n); return *this; }
+  outstream &operator<<(const __uint128_t &__n) { detail::Pr(__n); return *this; }
 #endif
   outstream &operator<<(const std::string &__n) {
-    for(const char &__c: __n) io::pc(__c);
+    for(const char &__c: __n) detail::pc(__c);
     return *this;
   }
 } out;
+}  // namespace io
+using io::in;
+using io::out;
