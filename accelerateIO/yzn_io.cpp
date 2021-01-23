57e2eecb6a7a1ea99b70e43f8ca5e733d60bf902
ConanYu <33613505+ConanYu@users.noreply.github.com>
2021-01-23 13:28:02 +0800

    fix: fix long int io not fount

--- accelerateIO/yzn_io.cpp
+++ accelerateIO/yzn_io.cpp
@@ -4,7 +4,7 @@ namespace io {
   inline char gc() { return ((st == ed) ? (st = buf, ed = st + fread(buf, 1, buflen, stdin), (st == ed) ? EOF : *st++) : *st++); }
   inline bool blank(char c) { return c == ' ' || c == '-' || c == '\n' || c == '\t' || c == '\r'; }
   template <typename T> inline bool Re(T &x) {
-    register char c; int f = 1;
+    char c; int f = 1;
     while (blank(c = gc())) if (c == '-') f = -1;
     if (c == EOF) return false;
     for (x = c - '0'; (c = gc()) >= '0' && c <= '9'; x = x * 10 + c - '0');
@@ -35,13 +35,15 @@ struct instream {
   instream &operator>>(unsigned short &__n) { io::Re(__n); return *this; }
   instream &operator>>(int &__n) { io::Re(__n); return *this; }
   instream &operator>>(unsigned int &__n) { io::Re(__n); return *this; }
+  instream &operator>>(long &__n) { io::Re(__n); return *this; }
+  instream &operator>>(unsigned long &__n) { io::Re(__n); return *this; }
   instream &operator>>(long long &__n) { io::Re(__n); return *this; }
   instream &operator>>(unsigned long long &__n) { io::Re(__n); return *this; }
 #ifdef __INT128__
   instream &operator>>(__int128 &__n) { io::Re(__n); return *this;}
   instream &operator>>(__uint128_t &__n) { io::Re(__n); return *this; }
 #endif
-  instream &operator>>(string &__n) {
+  instream &operator>>(std::string &__n) {
     __n.clear();
     char c = io::gc();
     if(c == EOF) return *this;
@@ -62,13 +64,15 @@ struct outstream {
   outstream &operator<<(const unsigned short &__n) { io::Pr(__n); return *this; }
   outstream &operator<<(const int &__n) { io::Pr(__n); return *this; }
   outstream &operator<<(const unsigned int &__n) { io::Pr(__n); return *this; }
+  outstream &operator<<(const long &__n) { io::Pr(__n); return *this; }
+  outstream &operator<<(const unsigned long &__n) { io::Pr(__n); return *this; }
   outstream &operator<<(const long long &__n) { io::Pr(__n); return *this; }
   outstream &operator<<(const unsigned long long &__n) { io::Pr(__n); return *this; }
 #ifdef __INT128__
   outstream &operator<<(const __int128 &__n) { io::Pr(__n); return *this; }
   outstream &operator<<(const __uint128_t &__n) { io::Pr(__n); return *this; }
 #endif
-  outstream &operator<<(const string &__n) {
+  outstream &operator<<(const std::string &__n) {
     for(const char &__c: __n) io::pc(__c);
     return *this;
   }
